6bf5ceca443c0dd812ef69507b801d1997109251
anurag.dutta <anurag.dutta@IRIS-CSG-2178.irissoftware.com>
2019-10-03 17:16:07 +0530

    DB insertion from controller

--- ItineraryAppBackend/src/main/java/com/iris/service/MyService.java
+++ ItineraryAppBackend/src/main/java/com/iris/service/MyService.java
@@ -1,5 +1,15 @@
 package com.iris.service;
 
+import org.springframework.stereotype.Service;
+
+import com.iris.entity.Client;
+import com.iris.entity.User;
+
+@Service
 public interface MyService {
+	
+//	public User validate(int userId);
+	
+	public Client addClient(Client cli);
 
 }
--- ItineraryAppBackend/src/main/java/com/iris/service/MyServiceImpl.java
+++ ItineraryAppBackend/src/main/java/com/iris/service/MyServiceImpl.java
@@ -1,5 +1,30 @@
 package com.iris.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.iris.entity.Client;
+import com.iris.entity.User;
+import com.iris.repository.ClientRepository;
+
+@Service
 public class MyServiceImpl implements MyService {
+	
+	@Autowired
+	ClientRepository dao;
+
+	@Override
+	public Client addClient(Client cli) {
+		System.out.println("Entering service");
+		return  dao.save(cli);
+	}
+
+//	@Override
+//	public User validate(int userId) {
+//		// TODO Auto-generated method stub
+//		return null;
+//	}
+
+	
 
 }
