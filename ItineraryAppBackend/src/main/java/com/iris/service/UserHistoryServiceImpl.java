17102e2413e6b3cd760012aab7103cf4dd321a88
abhishek.kundu <abhishek.kundu@IRIS-CSG-2032.irissoftware.com>
2019-10-04 11:37:17 +0530

    updated with userhistory func

--- ItineraryAppBackend/src/main/java/com/iris/service/UserHistoryServiceImpl.java
+++ ItineraryAppBackend/src/main/java/com/iris/service/UserHistoryServiceImpl.java
@@ -5,40 +5,43 @@ import java.util.List;
 import org.springframework.stereotype.Service;
 
 import com.iris.entity.Client;
+import com.iris.entity.User;
 
 @Service
-public class ClientServiceImpl implements ClientService{
+public class UserHistoryServiceImpl implements UserService{
 
 	@Override
-	public boolean addNewClient(Client c) {
+	public boolean addNewUser(User c) {
 		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
-	public boolean updateClient(Client c) {
+	public boolean updateUser(User c) {
 		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
-	public boolean deleteClient(Client c) {
+	public boolean deleteUser(User c) {
 		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
-	public Client viewClientById(int id) {
+	public User viewUserById(int id) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public List<Client> viewAllClients(int id) {
+	public List<User> viewAllUser() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	
 	
+	
+
 }
